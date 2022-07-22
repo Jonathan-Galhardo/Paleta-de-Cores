a65b8831a75c650f7a6bbeaf3f01a2d164e73139
Jonathan-Galhardo <galhardoj@gmail.com>
2022-07-22 11:30:33 -0300

    mudanca de cor com javascript

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,1 +1,12 @@
-//window.alert('Olá')
+function um(){
+    //window.alert('ok')
+    document.getElementById('body').style.background = "#CAD959"
+    document.getElementById('menu').style.background = "#1B8EF2"
+    document.getElementById('home').style.background = "#1A2E40"
+    document.getElementById('section1').style.background = "#CAD959"
+    document.getElementById('section2').style.background = "#B4BF5E"
+    document.getElementById('section3').style.background = "#CAD959"
+    document.getElementById('section4').style.background = "#B4BF5E"
+    document.getElementById('section5').style.background = "#CAD959"
+    document.getElementById('imgcapa').src = 'imagens/redes.jpg'
+} 
