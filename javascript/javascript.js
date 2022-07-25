fbb4f92751766ed325d146b30968862633e51362
Jonathan-Galhardo <galhardoj@gmail.com>
2022-07-25 16:13:16 -0300

    finalizacao

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,6 +1,7 @@
 function azul(){
     //window.alert('ok')
     document.getElementById('body').style.background = "#D5D7F2"
+    document.getElementById('body').style.color = "white" 
     document.getElementById('menu').style.background = "#1B21A6"
     document.getElementById('home').style.background = "#3D46F2"
     document.getElementById('section1').style.background = "#B4B6D9"
@@ -8,12 +9,13 @@ function azul(){
     document.getElementById('section3').style.background = "#B4B6D9"
     document.getElementById('section4').style.background = "#4178BF"
     document.getElementById('section5').style.background = "#B4B6D9"
-    //document.getElementById('imgcapa').src = 'imagens/redes.jpg'
+    document.getElementById('imgcapa').src = 'imagens/azul.jpg'
 } 
 
 function verde(){
     //window.alert('ok')
-    document.getElementById('body').style.background = "#B8D9BA"
+    document.getElementById('body').style.background = "#B8D9BA" 
+    document.getElementById('body').style.color = "white"
     document.getElementById('menu').style.background = "#09A603"
     document.getElementById('home').style.background = "#0ABF04"
     document.getElementById('section1').style.background = "#B8D9BA"
@@ -21,12 +23,13 @@ function verde(){
     document.getElementById('section3').style.background = "#B8D9BA"
     document.getElementById('section4').style.background = "#63BF6A"
     document.getElementById('section5').style.background = "#B8D9BA"
-    //document.getElementById('imgcapa').src = 'imagens/redes.jpg'
+    document.getElementById('imgcapa').src = 'imagens/verde.jpg'
 } 
 
 function laranja(){
     //window.alert('ok')
     document.getElementById('body').style.background = "#ffdab9"
+    document.getElementById('body').style.color = "white"
     document.getElementById('menu').style.background = "#ff4500"
     document.getElementById('home').style.background = "#ff8c00"
     document.getElementById('section1').style.background = "#ff7f50"
@@ -34,12 +37,13 @@ function laranja(){
     document.getElementById('section3').style.background = "#ff7f50"
     document.getElementById('section4').style.background = "#D93D04"
     document.getElementById('section5').style.background = "#ff7f50"
-    //document.getElementById('imgcapa').src = 'imagens/redes.jpg'
+    document.getElementById('imgcapa').src = 'imagens/laranja.jpg'
 } 
 
 function red(){
     //window.alert('ok')
     document.getElementById('body').style.background = "#ffa07a"
+    document.getElementById('body').style.color = "white"
     document.getElementById('menu').style.background = "#ff6347"
     document.getElementById('home').style.background = "#ff0000"
     document.getElementById('section1').style.background = "#e9967a"
@@ -47,5 +51,5 @@ function red(){
     document.getElementById('section3').style.background = "#e9967a"
     document.getElementById('section4').style.background = "#c90016"
     document.getElementById('section5').style.background = "#e9967a"
-    //document.getElementById('imgcapa').src = 'imagens/redes.jpg'
+    document.getElementById('imgcapa').src = 'imagens/vermelho.jpg'
 } 
